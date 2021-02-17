4200f6cac3c332bf6b478d56b27ca270a40f6a71
Mohammad Sakib Hossain <sakib.swe.diu@gmail.com>
2021-02-17 12:04:05 +0600

    4th release

--- dj_realestate/dj_realestate/settings.py
+++ dj_realestate/dj_realestate/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '*xx#rdsp2f(%*wh#pr&a91&-s%8p2ecoxjdi*(f^w^gfe$ek(q'
+SECRET_KEY = '*xx#rdsp2f(%*wh#pr&a91&-s%asjashgdcxcz*(f^w^gfe$ek(q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -124,6 +124,6 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')  # Copy from STATICFILES_DIRS
 STATICFILES_STORAGE='whitenoise.storage.CompressedManifestStaticFilesStorage'
-
+    # whitenoise static root theke static file niya server kora
 # Activate Django-Heroku.
 django_heroku.settings(locals())
